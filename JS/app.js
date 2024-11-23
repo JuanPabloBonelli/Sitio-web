9583ac51e970f25fd6abf2c0bda6f8a4b60428f1
Juan Pablo Bonelli <juampibonelli@hotmail.com>
2024-11-23 20:04:13 -0300

    Animacion h1

--- JS/app.js
+++ JS/app.js
@@ -7,10 +7,28 @@ toggle.addEventListener("change", (event) => {
     let main = document.querySelector('main'); 
     main.classList.toggle("dark");
     if (checked == true) {
-        toggle_label.innerHTML = '<i class="fa-solid fa-sun"></i>';
-        toggle_label.style.color="yellow";       
-    } else {
         toggle_label.innerHTML = '<i class="fa-solid fa-moon"></i>';
-        toggle_label.style.color="black";   
+        toggle_label.style.color="black";       
+    } else {
+        toggle_label.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        toggle_label.style.color="yellow";   
     }
 });
+
+
+// Animación del h1
+
+let texto = document.querySelector('.titulo'); //Query para traer del html una clase
+let contenido = 'A pottery studio dedicated to creativity and community.';
+let i = 0;
+
+function animacion(){
+    if (i < contenido.length){
+        texto.textContent += contenido.charAt(i)
+        i++
+        setTimeout(animacion, 100);
+    }
+
+}
+
+animacion()
