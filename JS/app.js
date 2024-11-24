0dfcbd621c63f3e42677ba0c231e3474b8591204
Juan Pablo Bonelli <juampibonelli@hotmail.com>
2024-11-24 11:34:57 -0300

    animación cartas

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,7 @@ let toggle = document.getElementById("toggle");
 let toggle_label = document.getElementById("toggle_label");
 toggle.addEventListener("change", (event) => {
     let checked = event.target.checked;
-    let main = document.querySelector('main'); 
+    let main = document.querySelector('main');   // Solo se aplica al main
     main.classList.toggle("dark");
     if (checked == true) {
         toggle_label.innerHTML = '<i class="fa-solid fa-moon"></i>';
@@ -32,3 +32,20 @@ function animacion(){
 }
 
 animacion()
+
+
+// Animación a las cartas
+const cards = document.querySelectorAll('.card'); // Llama a las 6 cartas por su clase
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {     // Mouse  enter hace referencia a cuando el cursor pasa por encima de la carta       
+    card.style.boxShadow = "6px 6px 6px #635f5f"; // Sombra con el mismo color que el modo oscuro
+    card.style.transform = 'scale(1.04)'; // Un aumento del 4%     
+  });
+
+  card.addEventListener('mouseleave', () => {  // Mouse leave hace referencia a cuando el cursor no se encuentra más sobre la carta    
+    card.style.boxShadow = "none";    // Quita de sombra
+    card.style.transform = 'scale(1)'; // Aumento original
+  });
+});
+
